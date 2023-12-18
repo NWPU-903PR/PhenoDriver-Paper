38757f36a634b419210e460fe4db34ca21366027
LiYan <linay0124@outlook.com>
2023-12-18 10:55:08 +0800

    fix bug

--- main.R
+++ main.R
@@ -4,16 +4,25 @@ library(AnnotationDbi)
 library(org.Hs.eg.db)
 
 # Inpute data
+setwd('~/Gungnir/')
 expN <- read_csv("./data/BRCA_NT_Rawcounts.csv")
 expT <- read_csv('./data/BRCA_TP_Rawcounts.csv')
 mutationData <- read_tsv('./data/MC3_BRCA.maf.gz', progress = TRUE, col_types = cols())
-reactome <- read.delim('./data/NCBI2Reactome.txt', header = F)
 STN <- read.delim('./data/SignalTransductionNet.txt')
+
+reactome <- read.delim('./data/NCBI2Reactome.txt', header = F)
+reactome <- reactome[reactome$V6 == 'Homo sapiens', 1:4]
+reactome <- unique(reactome)
+reactome$V1 <- AnnotationDbi::select(org.Hs.eg.db, keys = reactome$V1, columns = 'SYMBOL')$SYMBOL
+reactome <- unique(reactome)
+reactome <- reactome[!(is.na(reactome$V1)),]
+reactome <- reactome[reactome$V4 %in% names(table(reactome$V4)[table(reactome$V4) > 10 &
+                                                                 table(reactome$V4) < 200]),]
 
 # Data per-processing
 expressionData <- list(Normal = expN, Tumor = expT)
-cancerData <- dataPerProcessing(expressionData = expressionData,
-                                mutationData = mutationData,
+cancerData <- dataPerProcessing(expressionData = cancerData$expressionData,
+                                mutationData = cancerData$mutationData,
                                 annotationcol = 2,
                                 removeDuplicates = 2,
                                 removeSilenceSNP = F)
@@ -23,14 +32,6 @@ diffexpgene <- DEA(exp = cancerData$expressionData,
                    parallelworker = 10,
                    genenamecol = 2,
                    annotationcol = 2)
-
-reactome <- reactome[reactome$V6 == 'Homo sapiens', 1:4]
-reactome <- unique(reactome)
-reactome$V1 <- select(org.Hs.eg.db, keys = reactome$V1, columns = 'SYMBOL')$SYMBOL
-reactome <- unique(reactome)
-reactome <- reactome[!(is.na(reactome$V1)),]
-reactome <- reactome[reactome$V4 %in% names(table(reactome$V4)[table(reactome$V4) > 10 &
-                                                                 table(reactome$V4) < 200]),]
 
 # Get personalized abnormal pathways
 enrichreactomeRes <- pAbnormalPathway(diff = diffexpgene,
